124157a957ddda8d05ce99f8047e33aeb036f26d
said Lansarov <saidlansarov12@gmail.com>
2023-01-16 16:15:51 +0300

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            defef
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            fdf
+        </div>
+    );
+};
+
+export default Login;
